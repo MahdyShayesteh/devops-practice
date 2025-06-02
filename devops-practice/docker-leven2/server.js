c674976d011b2943acfbeea8f477ea2c6587c96c
mahdy shayesteh <mahdy.shayesteh@gmail.com>
2025-06-02 23:19:56 +0330

    changes in dokcerfile and server.js

--- devops-practice/docker-leven2/server.js
+++ devops-practice/docker-leven2/server.js
@@ -5,18 +5,17 @@ const path = require('path');
 const logFile = path.join(__dirname, 'logs', 'access.log');
 
 const server = http.createServer((req, res) => {
-    const log = '${new Date().toISOString()} - ${req.method} ${req.url}\n';
+  const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
+  
+  fs.appendFile(logFile, log, (err) => {
+    if (err) console.error('Error writing to log file:', err);
+  });
 
-    fs.appendFile(logFile, log, (err) => {
-        if (err) console.error('Error writing to log file:', err);
-    });
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from log server!\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from log server!\n');
 });
-
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
